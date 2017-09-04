81a984b184bfb947d1a72c9e859bb67d55d4ab48
Rob Adams <robert.andrew.adams@gmail.com>
2017-09-04 09:40:50 +0100

    Addnote requring function

--- src/components/addNote/index.js
+++ src/components/addNote/index.js
@@ -1,22 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const AddNote = () => (
-  <div>
-    <form>
-      <div className="note-Inputs">
-        <h4>Add new note</h4>
-        <div className="note-Input">
-          <input className="note-title" placeholder="Note Title"/>
+const AddNote = ({submitNote}) => {
+
+  let inputTitle;
+  let inputText;
+
+  return (
+    <div>
+      <form
+        onSubmit = {(event) => {
+          event.preventDefault();
+          submitNote(inputTitle.value, inputText.value);
+          inputTitle.value = '';
+          inputText.value = '';
+        }}
+        >
+        <div className="note-Inputs">
+          <h4>Add new note</h4>
+          <div className="note-Input">
+            <input className="note-title" placeholder="Note Title"
+            ref={(element) => {
+              inputTitle = element;}}
+            />
+          </div>
+          <div className="note-Input">
+            <input className="note-text" placeholder="Note Message"
+            ref={(element) => {
+              inputText = element;}}
+            />
+          </div>
         </div>
-        <div className="note-Input">
-          <input className="note-text" placeholder="Note Message"/>
-        </div>
-      </div>
-      <button type="submit" className="note-submit">
-        Add Note
-      </button>
-    </form>
-  </div>
-);
+        <button type="submit" className="note-submit">
+          Add Note
+        </button>
+      </form>
+    </div>
+  );
+};
+
+AddNote.propTypes = {
+  submitNote: PropTypes.func.isRequired,
+};
 
 export default AddNote;
