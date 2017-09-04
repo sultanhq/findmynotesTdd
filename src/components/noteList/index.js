db778dabf5631d7b9c80e9b15915be71357d476a
Rob Adams <robert.andrew.adams@gmail.com>
2017-09-04 19:20:49 +0100

    change note layout

--- src/components/noteList/index.js
+++ src/components/noteList/index.js
@@ -4,8 +4,9 @@ import PropTypes from 'prop-types';
 const NoteList = ({notes}) => {
   const noteItems = notes.map(note => (
     <li key={note.id}>
-      <span className="note-title">{note.title}</span>
-      <span className="note-text">{note.text}</span>
+      <div className="note-title"><h4>{note.title}</h4>
+          <div className="note-text">{note.text}</div>
+      </div>
     </li>
   ));
 
